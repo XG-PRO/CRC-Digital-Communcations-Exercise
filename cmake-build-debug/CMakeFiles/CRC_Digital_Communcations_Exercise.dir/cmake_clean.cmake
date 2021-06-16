ec08c63300a82ab72fa3154c8cc23a9ad09d33f5
XG-PRO <knperros@csd.auth.gr>
2021-06-16 19:09:35 +0300

    Initial Commit

---
+++ cmake-build-debug/CMakeFiles/CRC_Digital_Communcations_Exercise.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CRC_Digital_Communcations_Exercise.dir/main.cpp.obj"
+  "CRC_Digital_Communcations_Exercise.exe"
+  "CRC_Digital_Communcations_Exercise.exe.manifest"
+  "CRC_Digital_Communcations_Exercise.pdb"
+  "libCRC_Digital_Communcations_Exercise.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CRC_Digital_Communcations_Exercise.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,14 @@
+//
+// Created by XG-PRO on 6/16/2021.
+//
+#include <iostream>
+#include <cmath>
+#include<fstream>
+#include <cstring>
+
+using namespace std;
+
+int main(){
+    cout<<"Bruh";
+}
+
